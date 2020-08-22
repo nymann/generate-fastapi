c93b001b3854cbca2274705426dd7b128c92649f
Kristian Nymann Jakobsen <kristian@nymann.dev>
2020-08-22 11:15:51 +0200

    :bug: Apply fix for 'Optional is referenced as part of pydantic instead of typing'

--- src/generate_fastapi/mako_templates/src/project/domain/model/template_schemas.py
+++ src/generate_fastapi/mako_templates/src/project/domain/model/template_schemas.py
@@ -4,17 +4,18 @@ These schemas are used for creating new instances of ${model.names.singular_name
 paginated result (`Paginated`) and transforming a ${model.names.singular_name}
 
 """
+from typing import List, Optional
 import datetime
 from dataclasses import field
 from os import name
-from typing import List
 from xml.etree import ElementTree as ET
 
 import pydantic
 
 from ${PROJECT_NAME}.domain import base_schemas
 
-<%! from generate_fastapi.translators.json_translator import JsonTranslator %>
+< %! from generate_fastapi.translators.json_translator import JsonTranslator % >
+
 
 class _Base(pydantic.BaseModel):
     """Used as baseclass for all other schemas inside this module.
@@ -22,14 +23,15 @@ class _Base(pydantic.BaseModel):
     The base schema is kept private (by leftpadding with `_`).
     """
 
+
 % for field in model.fields:
     % if not field.is_primary_key:
     % if field.field_type.nullable:
-    ${field.name}: pydantic.Optional[${JsonTranslator.translate_typename_to_pytypes(field.field_type.name)}] = pydantic.Field(None${', max_length='+str(field.field_type.max_length) if field.field_type.max_length else ''}\
-${', min_length='+str(field.field_type.min_length) if field.field_type.min_length else ''})
+    ${field.name}: Optional[${JsonTranslator.translate_typename_to_pytypes(field.field_type.name)}] = pydantic.Field(None${', max_length='+str(field.field_type.max_length) if field.field_type.max_length else ''}
+                                                                                                                     ${', min_length='+str(field.field_type.min_length) if field.field_type.min_length else ''})
     % else:
-    ${field.name}: ${JsonTranslator.translate_typename_to_pytypes(field.field_type.name)} = pydantic.Field(...${', max_length='+str(field.field_type.max_length) if field.field_type.max_length else ''}\
-${', min_length='+str(field.field_type.min_length) if field.field_type.min_length else ''})
+    ${field.name}: ${JsonTranslator.translate_typename_to_pytypes(field.field_type.name)} = pydantic.Field(...${', max_length='+str(field.field_type.max_length) if field.field_type.max_length else ''}
+                                                                                                           ${', min_length='+str(field.field_type.min_length) if field.field_type.min_length else ''})
     %endif
     %endif
 % endfor
@@ -38,8 +40,10 @@ ${', min_length='+str(field.field_type.min_length) if field.field_type.min_lengt
 class Create(_Base):
     """Create schema is used for validating POST requests."""
 
+
 class Update(_Base):
     """Update schema is used for validating POST requests."""
+
 
 class DB(_Base):
     """DB schema is used for transforming an ORM model to a pydantic model."""
