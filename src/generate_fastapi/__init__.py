ab13b131d2080454dada5c2effc6039d1d2a21ba
Kristian Nymann Jakobsen <kristian@nymann.dev>
2020-09-05 13:48:45 +0200

    :bug: Fix wrong indentation

--- src/generate_fastapi/__init__.py
+++ src/generate_fastapi/__init__.py
@@ -16,7 +16,10 @@ from generate_fastapi.parsers import json_parser
 from generate_fastapi.parsers import sql_parser
 
 
-def gen_dirs_and_files(file: str, targetpath: str, project_name: str, git_repo_url: str = None):
+def gen_dirs_and_files(file: str,
+                       targetpath: str,
+                       project_name: str,
+                       git_repo_url: str = None):
     """gen_dirs_and_files.
 
     Args:
@@ -27,20 +30,17 @@ def gen_dirs_and_files(file: str, targetpath: str, project_name: str, git_repo_u
     """
     if not os.path.isdir(targetpath):
         sys.stdout.write(
-                'The target directory does not exist.' +
-                'Please try again with a different target path.', )
+            'The target directory does not exist.' +
+            'Please try again with a different target path.', )
         return
 
     if git_repo_url:
         Repo.clone_from(git_repo_url, targetpath)
 
     templates_path = str(
-            os.path.join(
-                pathlib.Path(__file__).parent.absolute(),
-                'mako_templates',
-                ))
-
-            models = []
+        os.path.join(
+            pathlib.Path(__file__).parent.absolute(), 'mako_templates'))
+    models = []
     if file.endswith('.sql'):
         models = sql_parser.parse_sql(file)
     if file.endswith('.json'):
@@ -48,35 +48,36 @@ def gen_dirs_and_files(file: str, targetpath: str, project_name: str, git_repo_u
 
     print(models)
     FastApiGenerator.gen_api_files(
-            models,
-            templates_path,
-            targetpath,
-            project_name,
-            )
+        models,
+        templates_path,
+        targetpath,
+        project_name,
+    )
 
 
-    file_arg: str = typer.Argument(
-            ...,
-            help="Path to a SQL upgrade migration or a JSON file")
+file_arg: str = typer.Argument(
+    ..., help="Path to a SQL upgrade migration or a JSON file")
 
-    target_dir_arg: str = typer.Option(
-            ".", help="Path to the target directory", prompt=True)
+target_dir_arg: str = typer.Option(".",
+                                   help="Path to the target directory",
+                                   prompt=True)
 
-    project_name_arg: str = typer.Option(
-            ...,
-            help="Your project name fx. 'my_program'"),
+project_name_arg: str = typer.Option(...,
+                                     help="Your project name fx. 'my_program'")
 
 
-    def main(file: str = file_arg,
-            target_directory: str = target_dir_arg,
-            project_name: str = project_name_arg,
-            git_repo_url: str = typer.Option(None, '--from_repo')):
-        """Main entrypoint for the application
+def main(file: str = file_arg,
+         target_directory: str = target_dir_arg,
+         project_name: str = project_name_arg,
+         git_repo_url: str = typer.Option(None, '--from_repo')):
+    """Main entrypoint for the application
     """
-    gen_dirs_and_files(file=file, targetpath=target_directory,
-            project_name=project_name, git_repo_url=git_repo_url)
+    gen_dirs_and_files(file=file,
+                       targetpath=target_directory,
+                       project_name=project_name,
+                       git_repo_url=git_repo_url)
 
 
-    def entry_point():
-        """entry_point [summary]."""
+def entry_point():
+    """entry_point [summary]."""
     typer.run(main)
